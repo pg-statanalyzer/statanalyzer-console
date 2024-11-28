5c84b73bb9674913e9f8ba9cf180dd01f469d28f
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2024-11-28 23:16:36 +0700

    aaaaaaaaaaaaaaaa

--- src/main/java/ru/postgrespro/perf/pgmicrobench/Main.java
+++ src/main/java/ru/postgrespro/perf/pgmicrobench/Main.java
@@ -1,6 +1,6 @@
 package ru.postgrespro.perf.pgmicrobench;
 
-import ru.postgrespro.perf.pgmicrobench.statanalyzer.distributions.PgDistribution;
+//import ru.postgrespro.perf.pgmicrobench.statanalyzer.distributions.PgDistribution;
 
 public class Main {
     public static void main(String[] args) {
